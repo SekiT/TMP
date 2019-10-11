b0a5c606b5f2405d3ff490e889b9ff19703d17f6
Takaaki Seki <takaaki.seki@access-company.com>
2019-10-11 17:09:09 +0900

    Use windowSize subject and tweak styles

--- view/tape.js
+++ view/tape.js
@@ -1,35 +1,43 @@
 import { wire } from 'hyperhtml/esm';
 import view from '../lib/view';
+import windowSize from '../subject/windowSize';
 
 const defaultState = {
   tape: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  position: 0,
+  position: 4,
+  width: 0,
 };
-
-const colors = ['black', 'white'];
 
 const cellDivs = defaultState.tape.map(() => wire({}));
 
-export default view(defaultState, (render) => ({ tape, position }) => {
+const tapeView = view(defaultState, (render) => ({ tape, position, width }) => {
   const containerStyle = {
-    position: 'relative',
-    left: `${position}px`,
+    position: 'absolute',
+    left: `${width * (5.25 - position)}px`,
+    width: `${width * 10}px`,
+    height: `${width}px`,
+  };
+  const cellStyle = {
+    display: 'table-cell',
+    width: `${width}px`,
+    height: `${width}px`,
+    'font-size': `${width * 0.8}px`,
+    'text-align': 'center',
   };
   return render`
   <div style=${containerStyle}>
   ${cellDivs.map((div, index) => {
     const character = tape[index];
-    const cellStyle = {
-      display: 'table-cell',
-      width: '30px',
-      height: '30px',
-      'text-align': 'center',
-      'vertical-align': 'middle',
-      'background-color': colors[character],
-      color: colors[1 - character],
-    };
-    return div`<div style=${cellStyle}>${character}</div>`;
+    const className = `cell${character}`;
+    return div`<div class=${className} style=${cellStyle}>${character}</div>`;
   })}
   </div>
   `;
 });
+
+windowSize.subscribe(({ width: windowWidth, height: windowHeight }) => {
+  const tapeWidth = Math.min(windowWidth * 0.08, windowHeight);
+  tapeView.update((state) => ({ ...state, width: tapeWidth }));
+});
+
+export default tapeView;
