78021a11e8735423f686e2c12f7f63b78f890f90
Takaaki Seki <takaaki.seki@access-company.com>
2019-10-14 21:26:33 +0900

    Reduce duplicated style declarations

--- view/machine/head.js
+++ view/machine/head.js
@@ -10,33 +10,34 @@ const headView = view(initialState, (render) => ({ state, cellWidth }) => {
   const containerStyle = {
     position: 'absolute',
     top: `${cellWidth}px`,
+    left: `${cellWidth * 5.1875}px`,
   };
   const commonStyle = {
     position: 'absolute',
     height: `${cellWidth}px`,
     background: 'radial-gradient(#AAAAAA, #666666)',
   };
+  const w16 = cellWidth * 0.0625;
   const armStyle = {
     ...commonStyle,
     top: '0px',
-    width: `${cellWidth * 0.125}px`,
-    'border-radius': `${cellWidth * 0.0625}px ${cellWidth * 0.0625}px 0 0`,
+    width: `${w16 * 2}px`,
+    'border-radius': `${w16}px ${w16}px 0 0`,
   };
   const headStyle = {
     ...commonStyle,
-    left: `${cellWidth * 5.1875}px`,
     top: `${cellWidth}px`,
     width: `${cellWidth * 1.125}px`,
-    'border-radius': `0 0 ${cellWidth * 0.0625}px ${cellWidth * 0.0625}px`,
-    color: 'white',
+    'border-radius': `0 0 ${w16}px ${w16}px`,
     'font-size': `${cellWidth * 0.8}px`,
     'line-height': `${cellWidth}px`,
     'text-align': 'center',
+    color: 'white',
   };
   return render`
     <div style=${containerStyle}>
-      <div style=${{ ...armStyle, left: `${cellWidth * 5.1875}px` }}></div>
-      <div style=${{ ...armStyle, left: `${cellWidth * 6.1875}px` }}></div>
+      <div style=${armStyle}></div>
+      <div style=${{ ...armStyle, left: `${cellWidth}px` }}></div>
       <div style=${headStyle}>${state}</div>
     </div>
   `;
