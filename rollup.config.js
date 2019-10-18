9f2d632a21ff73ecc9707dab056286ec3cf761a0
Takaaki Seki <takaaki.seki@access-company.com>
2019-10-18 15:26:50 +0900

    Save our time by livereload

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
 
 const commonPlugins = [
   resolve(),
@@ -10,6 +11,7 @@ const envDependentPlugins = process.env.MINIFY ? [
   terser(),
 ] : [
   serve(),
+  livereload(),
 ];
 
 export default {
