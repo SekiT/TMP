b4ec561bf5f047aafa3e53f89cffb6bf9aa07382
Takaaki Seki <takaaki.seki@access-company.com>
2019-10-18 15:21:51 +0900

    Introduce generator for reusable view definitions

---
+++ view/generator/tapeGen.js
@@ -0,0 +1,31 @@
+import { wire } from 'hyperhtml';
+import view from '../../lib/view';
+
+const colors = ['black', 'white'];
+
+export default () => {
+  const initialState = { tape: Array(10).fill(0), style: {} };
+  const cellDivs = [...Array(10)].map(() => wire({}));
+  return view(initialState, (render) => ({ tape, style, cellWidth }) => {
+    const tapeStyle = {
+      width: `${cellWidth * 10}px`,
+      height: `${cellWidth}px`,
+      ...style,
+    };
+    return render`<div style=${tapeStyle}>${
+      tape.map((bit, index) => {
+        const cellStyle = {
+          display: 'inline-block',
+          width: `${cellWidth}px`,
+          height: `${cellWidth}px`,
+          'line-height': `${cellWidth}px`,
+          'font-size': `${cellWidth}px`,
+          'text-align': 'center',
+          'background-color': colors[bit],
+          color: colors[1 - bit],
+        };
+        return cellDivs[index]`<div style=${cellStyle}>${bit}</div>`;
+      })
+    }</div>`;
+  });
+};
--- view/machine/tape.js
+++ view/machine/tape.js
@@ -1,39 +1,17 @@
-import { wire } from 'hyperhtml/esm';
-import view from '../../lib/view';
 import windowSize from '../../subject/windowSize';
+import tapeGen from '../generator/tapeGen';
 
-const defaultState = {
-  tape: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  position: 4,
-  width: 0,
-};
-
-const cellDivs = defaultState.tape.map(() => wire({}));
-
-const tapeView = view(defaultState, (render) => ({ tape, position, cellWidth }) => {
-  const containerStyle = {
-    position: 'absolute',
-    top: `${cellWidth}px`,
-    left: `${cellWidth * (5.75 - position)}px`,
-  };
-  const cellStyle = {
-    width: `${cellWidth}px`,
-    height: `${cellWidth}px`,
-    'font-size': `${cellWidth * 0.8}px`,
-    'line-height': `${cellWidth}px`,
-  };
-  return render`<div style=${containerStyle}>${
-    cellDivs.map((div, index) => {
-      const character = tape[index];
-      const className = `cell${character}`;
-      return div`<div class=${className} style=${cellStyle}>${character}</div>`;
-    })
-  }</div>`;
-});
+const tapeView = tapeGen();
 
 windowSize.subscribe(({ width: windowWidth, height: windowHeight }) => {
   const cellWidth = Math.min(windowWidth * 0.08, windowHeight / 3);
-  tapeView.update((state) => ({ ...state, cellWidth }));
+  const style = {
+    position: 'absolute',
+    top: `${cellWidth}px`,
+    left: `${cellWidth * 5.75}px`,
+    transform: `translate(${cellWidth * -4})`,
+  };
+  tapeView.update((state) => ({ ...state, style, cellWidth }));
 });
 
 export default tapeView;
