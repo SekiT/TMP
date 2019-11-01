2c0ff40ef950fdb5004176e50ccdc08215e348ad
SekiT <takaaki.seki@access-company.com>
2019-11-01 10:12:19 +0900

    Define process.env.TEST by rollup replace plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
+import replace from '@rollup/plugin-replace';
 
 const commonPlugins = [
   resolve(),
+  replace({ 'process.env.TEST': false }),
 ];
 
 const envDependentPlugins = process.env.PRODUCTION ? [
--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,8 +1,12 @@
 import multiEntry from 'rollup-plugin-multi-entry';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: 'test/**/*.js',
-  plugins: [multiEntry()],
+  plugins: [
+    multiEntry(),
+    replace({ 'process.env.TEST': true }),
+  ],
   output: { format: 'cjs' },
   external: ['tape'],
 };
