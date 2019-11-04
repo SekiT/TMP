0ab7dd3cfeb19c786fff83454d3c490c0eadbd1c
Takaaki Seki <takaaki.seki@access-company.com>
2019-11-04 10:50:08 +0900

    Date.now() instead of new Date().getTime()

--- test/lib/runPhaseTest.js
+++ test/lib/runPhaseTest.js
@@ -1,6 +1,6 @@
 import { test } from 'tape';
 import dependencies from 'dependencies';
-import { mockFunctionSequence, mockConstructor, resetAllMocks } from 'lib/shadow';
+import { mockPropertyGetter, mockFunctionSequence, resetAllMocks } from 'lib/shadow';
 import { runPhase, idealTimeout } from 'lib/runPhase';
 
 const { globals } = dependencies;
@@ -17,11 +17,13 @@ test('runPhase runs phase with balancing timeout', (t) => {
   t.plan(cases.length * 4);
   cases.forEach(([timeTaken, timeoutBefore, timeoutAfter]) => {
     let getTimeCount = 0;
-    mockConstructor(globals, 'Date', () => function MockDate() {
+    mockPropertyGetter(globals, 'Date', (original, key) => {
+      if (key !== 'now') return t.fail(`Unknown access to Date.${key}`);
       getTimeCount += 1;
-      if (getTimeCount === 3) t.pass('new Date() called 3 times');
-      if (getTimeCount >= 4) return t.fail('new Date() called too many times!');
-      this.getTime = () => [0, timeTaken, timeTaken][getTimeCount - 1];
+      if (getTimeCount === 3) t.pass('Date.now read 3 times');
+      if (getTimeCount > 3) return t.fail('Date.now read too many times!');
+      const now = [0, timeTaken, timeTaken][getTimeCount - 1];
+      return () => now;
     });
     const phase3 = () => t.fail('phase3 should not be called');
     const phase2 = () => { t.pass('phase2 called'); return phase3; };
