b5d5e5cc98dd334bc7eb5c290a8b73dde121aed1
Takaaki Seki <takaaki.seki@access-company.com>
2019-11-05 11:01:59 +0900

    Display timeLeft

--- phase/main/index.js
+++ phase/main/index.js
@@ -1,11 +1,18 @@
+import numbersView from 'view/case/numbers';
 import programWindowAppearing from './programWindowAppearing';
+
+const TIME_LIMIT = 32;
 
 const initialState = {
   subPhase: programWindowAppearing(0),
+  startedAt: null,
 };
 
-const mainPhase = ({ subPhase }) => () => (
-  mainPhase({ subPhase: subPhase() })
-);
+const mainPhase = ({ subPhase, startedAt: nullableStartedAt }) => () => {
+  const startedAt = nullableStartedAt || Date.now();
+  const timeLeft = Math.max(TIME_LIMIT - (Date.now() - startedAt) / 1000, 0);
+  numbersView.update(() => ({ timeLeft }));
+  return mainPhase({ subPhase: subPhase(), startedAt });
+};
 
 export default mainPhase(initialState);
