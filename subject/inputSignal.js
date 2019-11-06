481c34bf28db8d41ad131f72602049af79f68a31
SekiT <takaaki.seki@access-company.com>
2019-11-06 11:50:24 +0900

    Introduce inputSignal subject

---
+++ subject/inputSignal.js
@@ -0,0 +1,21 @@
+import subject from 'lib/subject';
+
+const inputSignalSubject = subject([]);
+
+export const enqueue = (signal) => inputSignalSubject.next((queue) => [...queue, signal]);
+
+// Note that dequeue() may be undefined.
+export const dequeue = () => {
+  let result;
+  inputSignalSubject.next(([head, ...tail]) => {
+    result = head;
+    return tail;
+  });
+  return result;
+};
+
+export const signals = {
+  reset: {},
+  skip: {},
+  run: {},
+};
