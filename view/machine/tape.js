2bb5794a3ab10732fbc7f65c16d32fc8da8e37ac
SekiT <takaaki.seki@access-company.com>
2019-11-06 10:38:25 +0900

    Make tapeView mobile

--- view/machine/tape.js
+++ view/machine/tape.js
@@ -1,17 +1,27 @@
+import view from 'lib/view';
 import windowSize from 'subject/windowSize';
 import tapeGen from 'view/generator/tapeGen';
 
 const tapeView = tapeGen();
 
-windowSize.subscribe(({ width: windowWidth, height: windowHeight }) => {
-  const cellWidth = Math.min(windowWidth * 0.15, windowHeight * 0.3);
-  const style = {
+const containerView = view({ position: 0 }, (render) => ({
+  position, cellWidth, ...tapeProps
+}) => {
+  const containerStyle = {
     position: 'absolute',
     top: '30%',
     left: '50%',
-    transform: `translate(-${cellWidth / 2}px, 0)`,
+    transform: `translate(${cellWidth * (-0.5 - position)}px, 0)`,
   };
-  tapeView.update(() => ({ style, cellWidth }));
+  tapeView.update(() => ({ ...tapeProps, cellWidth }));
+  return render`<div style=${containerStyle}>
+    ${tapeView.render()}
+  </div>`;
 });
 
-export default tapeView;
+windowSize.subscribe(({ width: windowWidth, height: windowHeight }) => {
+  const cellWidth = Math.min(windowWidth * 0.15, windowHeight * 0.3);
+  containerView.update(() => ({ cellWidth }));
+});
+
+export default containerView;
