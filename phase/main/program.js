77ed047932023bd5e2e85f3ea2a756d2296bdde2
SekiT <takaaki.seki@access-company.com>
2019-11-07 19:08:12 +0900

    Show time

--- phase/main/program.js
+++ phase/main/program.js
@@ -3,12 +3,15 @@ import { programSubject, initialState } from 'subject/program';
 import { updateOrder } from 'view/case/tapes';
 import controlView from 'view/control/control';
 import ids from '../ids';
-import { FRAMES_TO_SWITCH_WINDOW, animateProgramWindow, animateTape } from './animations';
+import {
+  FRAMES_TO_SWITCH_WINDOW, animateProgramWindow, animateTape, showTime,
+} from './animations';
 
-export const programWindowClosing = (time) => ({ currentTape, displayedTape }) => {
+export const programWindowClosing = (time) => ({ currentTape, displayedTape, startedAt }) => {
   animateProgramWindow(time, true);
   animateTape(currentTape, displayedTape);
   controlView.update(() => ({ disabled: time > 0 }));
+  showTime(startedAt);
   return time === 0 ? {
     nextId: ids.main.programWindowClosing,
     nextArgs: [0],
@@ -18,8 +21,9 @@ export const programWindowClosing = (time) => ({ currentTape, displayedTape }) =
   };
 };
 
-export const programming = () => ({ currentTape, displayedTape }) => {
+export const programming = () => ({ currentTape, displayedTape, startedAt }) => {
   animateTape(currentTape, displayedTape);
+  showTime(startedAt);
   const signal = dequeue();
   if (signal === signals.run) {
     return {
@@ -37,13 +41,14 @@ export const programming = () => ({ currentTape, displayedTape }) => {
 };
 
 export const programWindowOpening = (time) => ({
-  order, originalTape, currentTape, displayedTape,
+  order, originalTape, currentTape, displayedTape, startedAt,
 }) => {
   updateOrder(order);
   const moving = time < FRAMES_TO_SWITCH_WINDOW;
   animateProgramWindow(time, moving);
   animateTape(currentTape, displayedTape);
   controlView.update(() => ({ disabled: moving }));
+  showTime(startedAt);
   return moving ? {
     nextId: ids.main.programWindowOpening,
     nextArgs: [time + 1],
