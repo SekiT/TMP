fa2979c6f02e1ab25ea9b3afa8dfeff7c08c4c87
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 17:02:40 +0900

    tapesView

--- view/case/tapes.js
+++ view/case/tapes.js
@@ -1,5 +1,6 @@
+import { css } from '@linaria/core';
 import tapeSubject from '@/subject/tape';
-import { view, toCssText } from '@/lib/view';
+import { view } from '@/lib/view';
 import tapeGen from '@/view/generator/tapeGen';
 
 const orderView = tapeGen(5, 7);
@@ -7,22 +8,22 @@ const tapeView = tapeGen(5, 7);
 
 export const updateOrder = (tape) => orderView.update(() => ({ tape }));
 
-const containerStyle = toCssText({
-  position: 'absolute',
-  top: '3%',
-  width: '100%',
-  fontFamily: 'serif',
-  color: 'white',
-});
+const container = css`
+  position: absolute;
+  top: 3%;
+  width: 100%;
+  font-family: serif;
+  color: white;
+`;
 
-const tableStyle = toCssText({
-  margin: '0 auto',
-  fontSize: 'min(3vw, 4.2vh)',
-  lineHeight: 'min(3vw, 4.2vh)',
-});
+const table = css`
+  margin: 0 auto;
+  font-size: min(3vw, 4.2vh);
+  line-height: min(3vw, 4.2vh);
+`;
 
-const tapesView = view({}, (render) => () => render`<div style=${containerStyle}>
-  <table style=${tableStyle}>
+const tapesView = view({}, (render) => () => render`<div class=${container}>
+  <table class=${table}>
     <tr><td>ORDER:</td><td>${orderView.render()}</td></tr>
     <tr><td>TAPE:</td><td>${tapeView.render()}</td></tr>
   </table>
