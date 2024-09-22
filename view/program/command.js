2670e9c80da1fce95e6ab6e485000183a69fb9f4
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 21:44:34 +0900

    commandView

--- view/program/command.js
+++ view/program/command.js
@@ -1,4 +1,5 @@
-import { view, toCssText } from '@/lib/view';
+import { css } from '@linaria/core';
+import { view } from '@/lib/view';
 import { updateCommand } from '@/subject/program';
 
 const initialState = (state0, char0) => ({
@@ -10,12 +11,12 @@ const initialState = (state0, char0) => ({
   disabled: false,
 });
 
-const containerStyle = toCssText({
-  display: 'inline-block',
-  margin: '1% 1%',
-  textAlign: 'center',
-  whiteSpace: 'nowrap',
-});
+const container = css`
+  display: inline-block;
+  margin: 1% 1%;
+  text-align: center;
+  white-space: nowrap;
+`;
 
 const directionToString = (direction) => (direction < 0 ? 'L' : 'R');
 const stateStrings = ['0', '1', '2', '3', '4', 'A'];
@@ -32,12 +33,8 @@ const updateState1 = (index, state1) => () => {
 
 export default (index) => view(initialState(index >> 1, index & 0), (render) => ({
   state0, char0, direction, char1, state1, disabled,
-}) => render`<div style=${containerStyle}>${
-  `<${state0},${char0},`
-}<button onclick=${updateDirection(index, direction)} .disabled=${disabled}>${directionToString(direction)}</button>${
-  ','
-}<button onclick=${updateChar1(index, char1)} .disabled=${disabled}>${char1}</button>${
-  ','
-}<button onclick=${updateState1(index, state1)} .disabled=${disabled}>${stateStrings[state1]}</button>${
-  '>'
-}</div>`);
+}) => render`<div class=${container}>${`<${state0},${char0},`
+  }<button onclick=${updateDirection(index, direction)} .disabled=${disabled}>${directionToString(direction)}</button>${','
+  }<button onclick=${updateChar1(index, char1)} .disabled=${disabled}>${char1}</button>${','
+  }<button onclick=${updateState1(index, state1)} .disabled=${disabled}>${stateStrings[state1]}</button>${'>'
+  }</div>`);
