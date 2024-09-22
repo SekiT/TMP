b10fe0e248ec4de0f8aa1875cbe7e762f6fb7fdd
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 13:11:52 +0900

    Bundle and emit as styles.css

--- rollup-config/index.mjs
+++ rollup-config/index.mjs
@@ -2,12 +2,16 @@ import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
+import css from 'rollup-plugin-css-only';
+import wyw from '@wyw-in-js/rollup';
 import alias from './alias.mjs';
 import minifyTemplate from './minifyTemplate.mjs';
 
 const commonPlugins = [
   resolve(),
   alias('index'),
+  wyw({ sourceMap: !process.env.PRODUCTION }),
+  css({ output: 'styles.css' }),
 ];
 
 const envDependentPlugins = process.env.PRODUCTION ? [
