4ad1cd3c34a917157f3c7042ce6688168309b916
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 20:47:29 +0900

    Revert "tapesView"
    This reverts commit fa2979c6f02e1ab25ea9b3afa8dfeff7c08c4c87.

--- view/case/tapes.js
+++ view/case/tapes.js
@@ -1,6 +1,5 @@
-import { css } from '@linaria/core';
 import tapeSubject from '@/subject/tape';
-import { view } from '@/lib/view';
+import { view, toCssText } from '@/lib/view';
 import tapeGen from '@/view/generator/tapeGen';
 
 const orderView = tapeGen(5, 7);
@@ -8,22 +7,22 @@ const tapeView = tapeGen(5, 7);
 
 export const updateOrder = (tape) => orderView.update(() => ({ tape }));
 
-const container = css`
-  position: absolute;
-  top: 3%;
-  width: 100%;
-  font-family: serif;
-  color: white;
-`;
+const containerStyle = toCssText({
+  position: 'absolute',
+  top: '3%',
+  width: '100%',
+  fontFamily: 'serif',
+  color: 'white',
+});
 
-const table = css`
-  margin: 0 auto;
-  font-size: min(3vw, 4.2vh);
-  line-height: min(3vw, 4.2vh);
-`;
+const tableStyle = toCssText({
+  margin: '0 auto',
+  fontSize: 'min(3vw, 4.2vh)',
+  lineHeight: 'min(3vw, 4.2vh)',
+});
 
-const tapesView = view({}, (render) => () => render`<div class=${container}>
-  <table class=${table}>
+const tapesView = view({}, (render) => () => render`<div style=${containerStyle}>
+  <table style=${tableStyle}>
     <tr><td>ORDER:</td><td>${orderView.render()}</td></tr>
     <tr><td>TAPE:</td><td>${tapeView.render()}</td></tr>
   </table>
