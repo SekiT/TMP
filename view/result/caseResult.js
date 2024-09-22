8a0b44726fb7967607dc1e2676ca25fb35cc4dda
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 21:30:38 +0900

    caseResult

--- view/result/caseResult.js
+++ view/result/caseResult.js
@@ -1,5 +1,6 @@
+import { css } from '@linaria/core';
 import { TIME_LIMIT } from '@/constant';
-import { view, toCssText } from '@/lib/view';
+import { view } from '@/lib/view';
 import { showTime, showScore } from '../case/numbers';
 
 export const types = {
@@ -17,16 +18,21 @@ const initialState = {
   opacity: 0,
 };
 
-const containerStyle = (opacity) => toCssText({
-  display: opacity === 0 ? 'none' : 'block',
-  position: 'absolute',
-  top: '10%',
-  width: '100%',
-  textAlign: 'center',
-  color: 'white',
-  opacity,
-  filter: 'drop-shadow(0 0 0.3rem black)',
-});
+const container = css`
+  display: var(--display);
+  position: absolute;
+  top: 10%;
+  width: 100%;
+  text-align: center;
+  color: white;
+  opacity: var(--opacity);
+  filter: drop-shadow(0 0 0.3rem black);
+`;
+
+const containerStyle = (opacity) => `
+--display:${opacity ? 'block' : 'none'};
+--opacity:${opacity};
+`;
 
 const titleColor = new Map([
   [types.pass, '#ccc'],
@@ -36,10 +42,14 @@ const titleColor = new Map([
 
 const fontSize = 'min(4vw, 6vh)';
 
-const titleStyle = (type) => toCssText({
-  color: titleColor.get(type),
-  fontSize: `calc(${fontSize} * 1.3)`,
-});
+const title = css`
+  color: var(--titleColor);
+  font-size: calc(${fontSize} * 1.3);
+`;
+
+const titleStyle = (type) => `
+--titleColor:${titleColor.get(type)};
+`;
 
 const titleText = new Map([
   [types.pass, 'Pass'],
@@ -47,14 +57,14 @@ const titleText = new Map([
   [types.timeup, "Time's up."],
 ]);
 
-const scoreBoardStyle = toCssText({
-  display: 'inline-block',
-  margin: `${fontSize} 0`,
-  fontSize,
-  color: 'white',
-});
+const scoreBoard = css`
+  display: inline-block;
+  margin: ${fontSize} 0;
+  font-size: ${fontSize};
+  color: white;
+`;
 
-const bonusStyle = toCssText({ fontSize });
+const bonusClass = css`font-size: ${fontSize}`;
 
 export const bonus = (commandsSaved, accepted, steps, timeLeft) => (
   (commandsSaved + (accepted ? 1 : 0)) * 100 * steps * 0.1 * (timeLeft / TIME_LIMIT) * 3 | 0
@@ -63,9 +73,9 @@ export const bonus = (commandsSaved, accepted, steps, timeLeft) => (
 const caseResultView = view(initialState, (render) => ({
   type, commandsSaved, accepted, steps, timeLeft, opacity,
 }) => (
-  render`<div style=${containerStyle(opacity)}>
-    <div style=${titleStyle(type)}>${titleText.get(type)}</div>
-    <table style=${scoreBoardStyle}>
+  render`<div class=${container} style=${containerStyle(opacity)}>
+    <div class=${title} style=${titleStyle(type)}>${titleText.get(type)}</div>
+    <table class=${scoreBoard}>
       <tr>
         <td>Saved + accepted</td>
         <td />
@@ -81,9 +91,8 @@ const caseResultView = view(initialState, (render) => ({
         <td>(${showTime(timeLeft)} / ${TIME_LIMIT}.00 &times; 3)</td>
       </tr>
     </table>
-    <div style=${bonusStyle}>Score: +${
-    showScore(bonus(commandsSaved, accepted, steps, timeLeft))
-  }</div>
+    <div class=${bonusClass}>Score: +${showScore(bonus(commandsSaved, accepted, steps, timeLeft))
+    }</div>
   </div>`
 ));
 
