cab9eed60f366f05b30ea4c3d416af9306ae48cf
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 17:05:08 +0900

    controlView

--- view/control/control.js
+++ view/control/control.js
@@ -1,38 +1,39 @@
+import { css } from '@linaria/core';
 import { enqueue, signals } from '@/subject/inputSignal';
-import { view, toCssText } from '@/lib/view';
+import { view } from '@/lib/view';
 
 const initialState = {
   running: false,
   disabled: true,
 };
 
-const containerStyle = toCssText({
-  display: 'table-cell',
-  position: 'absolute',
-  top: '19%',
-  width: '100%',
-  height: '10%',
-  textAlign: 'center',
-  verticalAlign: 'middle',
-  fontFamily: 'Courier New',
-});
+const container = css`
+  display: table-cell;
+  position: absolute;
+  top: 19%;
+  width: 100%;
+  height: 10%;
+  text-align: center;
+  vertical-align: middle;
+  font-family: Courier New;
+`;
 
 const fontSize = 'min(5vw, 6.5vh)';
 
-const buttonStyle = toCssText({
-  margin: `calc(5vh - ${fontSize} / 2) 0.5em`,
-  width: `calc(${fontSize} * 5)`,
-  height: fontSize,
-  fontSize,
-  borderRadius: `calc(${fontSize} * 0.15)`,
-});
+const buttonStyle = css`
+  margin: calc(5vh - ${fontSize} / 2) 0.5em;
+  width: calc(${fontSize} * 5);
+  height: ${fontSize};
+  font-size: ${fontSize};
+  border-radius: calc(${fontSize} * 0.15);
+`;
 
 const onClickResetButton = () => enqueue(signals.reset);
 const onClickHaltButton = () => enqueue(signals.halt);
 const onClickPassButton = () => enqueue(signals.pass);
 
 const controlView = view(initialState, (render) => ({ running, disabled }) => (
-  render`<div style=${containerStyle}>
+  render`<div class=${container}>
     <button
       style=${buttonStyle}
       onclick=${running ? onClickHaltButton : onClickResetButton}
