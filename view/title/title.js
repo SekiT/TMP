d06241d9b54d489061c99a3307bcc1765f727188
Takaaki Seki <takaaki.seki@access-company.com>
2024-09-22 13:12:34 +0900

    Use linaria in titleView

--- view/title/title.js
+++ view/title/title.js
@@ -1,25 +1,31 @@
-import { view, toCssText } from '@/lib/view';
+import { css } from '@linaria/core';
+import { view } from '@/lib/view';
 
 const initialState = { opacity: 0 };
 
-const containerStyle = (opacity) => toCssText({
-  display: opacity === 0 ? 'none' : 'block',
-  position: 'absolute',
-  width: '100%',
-  top: '30%',
-  textAlign: 'center',
-  color: 'white',
-  opacity,
-});
+const container = css`
+  display: var(--display);
+  position: absolute;
+  width: 100%;
+  top: 30%;
+  text-align: center;
+  color: white;
+  opacity: var(--opacity);
+`;
 
-const titleStyle = toCssText({
-  fontSize: 'min(12vw, 18vh)',
-  color: 'orange',
-});
+const containerStyle = (opacity) => `
+--display:${opacity ? 'block' : 'none'};
+--opacity:${opacity};
+`;
+
+const title = css`
+  font-size: min(12vw, 18vh);
+  color: orange;
+`;
 
 const titleView = view(initialState, (render) => ({ opacity }) => (
-  render`<div style=${containerStyle(opacity)}>
-    <div style=${titleStyle}>TMP</div>
+  render`<div class=${container} style=${containerStyle(opacity)}>
+    <div class=${title}>TMP</div>
   </div>`
 ));
 
